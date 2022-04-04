7e635a5fd1ce33c10b53a89034a4c64a87ab93ca
marcusj12 <marcus.a.jennings12@gmail.com>
2022-04-04 19:42:04 -0400

    pet Shop cLi

--- src/com/careerdevs/PetShop.java
+++ src/com/careerdevs/PetShop.java
@@ -5,38 +5,55 @@ import java.util.List;
 public class PetShop {
     private String petShopName;
     private List<PetShop> availableAnimals;
-    private List<PetShop>soldAnimals;
+    private List<PetShop> soldAnimals;
 
-    // We are essentially using constructors to make up the parameters of the petshop object; objects mimic real world thingsm3
+    // We are essentially using constructors to make up the parameters of the pet shop object; objects mimic real world things
     public PetShop(String petShopName, List<PetShop> availableAnimals, List<PetShop> soldAnimals) {
         this.petShopName = petShopName;
         this.availableAnimals = availableAnimals;
         this.soldAnimals = soldAnimals;
 
-
     }
 
-    public String getPetShopName () {
-        return getPetShopName();
+    public String getPetShopName() {
+        return petShopName;
     }
-    public void setPetsShopName (String petShopName) {
+
+    public void setPetsShopName(String petShopName) {
         this.petShopName = petShopName;
     }
 
-    public List<PetShop> getAvailableAnimals () {
-        return getAvailableAnimals();
+    public List<PetShop> getAvailableAnimals() {
+        return availableAnimals;
     }
-    public void setAvailableAnimals (List<PetShop> availableAnimals) {
+
+    public void setAvailableAnimals(List<PetShop> availableAnimals) {
         this.availableAnimals = availableAnimals;
     }
 
-    public List<PetShop> getSoldAnimals(){
+    public List<PetShop> getSoldAnimals() {
         return soldAnimals;
     }
 
-    public void setSoldAnimals (List<PetShop> soldAnimals) {
+    public void setSoldAnimals(List<PetShop> soldAnimals) {
         this.soldAnimals = soldAnimals;
     }
 
+    @Override
+    public String toString() {
+        return "petShopName='" + petShopName + '\'' + "availableAnimals='" + availableAnimals + '\'' +
+                "soldAnimals='" + soldAnimals;
+    }
 
+    public void buyAnimal (String petName) {
+        for (int i = 0; i < availableAnimals.size(); i++) {
+            if (petName.equals(availableAnimals.size(i).getPetName())) {
+                availableAnimals.add((PetShop) soldAnimals);
+            }
+
+
+        }
+    }
 }
+
+
--- src/com/careerdevs/Pets.java
+++ src/com/careerdevs/Pets.java
@@ -4,15 +4,56 @@ public class Pets {
     private String petName;
     private String petSpecies;
     private int petAge;
-    private int price;
+    private int petPrice;
 
 
-    public Pets(String petName, String petSpecies, int price) {
+    public Pets(String petName, String petSpecies,int petAge, int petPrice) {
         this.petName = petName;
         this.petSpecies = petSpecies;
         this.petAge = petAge;
-        this.price = price;
+        this.petPrice = petPrice;
 
     }
 
+    public String getPetName (){
+        return getPetName();
+    }
+
+    public void setPetName(){
+        this.petName = petName;
+    }
+
+    public String getPetSpecies(){
+        return getPetSpecies();
+    }
+
+    public void setPetSpecies(){
+        this.petSpecies = petSpecies;
+    }
+
+    public int getPetAge(){
+        return petAge;
+    }
+
+    public void setPetAge(){
+        this.petAge = petAge;
+    }
+
+    public int getPetPrice(){
+        return petPrice;
+    }
+
+    public void setPetPrice(){
+        this.petPrice = petPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Pets{" +
+                "petName='" + petName + '\'' +
+                ", petSpecies='" + petSpecies + '\'' +
+                ", petAge=" + petAge +
+                ", petPrice=" + petPrice +
+                '}';
+    }
 }
